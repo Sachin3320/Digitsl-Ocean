65c0ee0a2a795dba3771fa02fc32f19e736c5a6d
Anonymous <rahul@pearlorganisation.com>
2025-03-19 15:00:34 +0530

    contact us

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
+import ContactUs from "./Pages/ContactUs";
 
 const App = () => {
   return (
     <div className="w-full">
-      <Home/>
+      {/* Navigation */}
+      <nav className="bg-gray-800 p-4 text-white">
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/" className="hover:underline">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:underline">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Page Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/Pages/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import From from '../Components/ContactUs/From'
+
+const ContactUs = () => {
+  return (
+    <div className='w-full h-full'>
+      <From/>
+      
+    </div>
+  )
+}
+
+export default ContactUs
